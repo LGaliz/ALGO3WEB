8cd88cc9953ba87ea63e98b38e61aa4afcaa98ea
mosjim <mosjim@gmail.com>
2017-09-28 19:13:30 -0300

    Iniciando Dominio JS

---
+++ src/js/Entrenador.js
@@ -0,0 +1,28 @@
+class Entrenador {
+    constructor() {
+        this.id
+        this.monedas
+        this.nombre
+        this.correoElectronico
+        this.List <  > notificacionesRecibidas = newArrayList
+        this.ubicacion
+        this.combatesGanados = 0
+        this.pokemonEvolucionados = 0
+        this.List < Especie > especiesCapturadas = newArrayList
+        this.experiencia = 0
+        this.perfilEntrenador
+        this.Map < Item,  > inventario = new HashMap < Item,
+             >
+            this.List < Pokemon > equipo = newArrayList
+        this.List < Pokemon > deposito = newArrayList
+        Randomizer randomizer = Randomizer.getInstance()
+        this.Set < Entrenador > amigos = newHashSet
+        this.Set < Entrenador > solicitudesDeAmistadPendientes = newHashSet
+        this.Set < NivelObserver > nivelObservers = newHashSet
+
+
+    }
+
+
+
+}
